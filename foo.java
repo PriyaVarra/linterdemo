80becfb122e0af469498fafc1b71d601ff10a03c
PriyaVarra <40502361+PriyaVarra@users.noreply.github.com>
2020-06-03 14:22:18 -0700

    Update foo.java

--- foo.java
+++ foo.java
@@ -1,10 +1,10 @@
 public class foo {
-  /** Prints some stuff */
+  /** Prints some stuff. */
   public static void main(String[] args) { 
     System.out.println("foo");
   
     if (1 < 2) {
       System.out.println("bar");
     }
-}
+  }
 }
