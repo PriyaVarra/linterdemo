2b3aec8114c398059066d89a710320dc39dc36cc
PriyaVarra <40502361+PriyaVarra@users.noreply.github.com>
2020-06-03 13:36:49 -0700

    Create foo.java

---
+++ foo.java
@@ -0,0 +1,12 @@
+public class foo {
+  public static void main(String[] args) {
+      if (1 < 2) 
+      {
+        System.out.println("Hello");
+      }
+      else 
+      {
+        System.out.println("Goodbye");
+      }
+  }
+}
