d8923e87caa0d82be40a6a76eba330b6d3db0586
PriyaVarra <40502361+PriyaVarra@users.noreply.github.com>
2020-06-03 14:18:38 -0700

    Update foo.java

--- foo.java
+++ foo.java
@@ -1,9 +1,9 @@
 public class foo {
   public static void main(String[] args) { 
-    
-      System.out.println("foo");
-    
-      if (1 < 2) 
-        System.out.println("bar");
-  }
+    System.out.println("foo");
+  
+    if (1 < 2) {
+      System.out.println("bar");
+    }
+}
 }
